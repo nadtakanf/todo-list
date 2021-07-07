8d59dff0b49139715ea8599c08b2bcbdd4036b85
nadtakanjones <nadtakan.jones@gmail.com>
2021-07-07 12:40:41 -0700

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const slsw = require('serverless-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  target: 'node',
+  entry: slsw.lib.entries,
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  node: false,
+  optimization: {
+    minimize: false,
+  },
+  devtool: 'inline-cheap-module-source-map',
+};

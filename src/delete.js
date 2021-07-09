d56fea498fbeae78be06d3d1452fb73acd579db0
nadtakanjones <nadtakan.jones@gmail.com>
2021-07-08 22:10:31 -0700

    delete functionality

--- src/delete.js
+++ src/delete.js
@@ -1,6 +1,28 @@
 'use strict';
 const log = require('lambda-log');
+const { DynamoDBClient, DeleteItemCommand } = require("@aws-sdk/client-dynamodb")
+const ddbClient = new DynamoDBClient({ region: process.env.REGION })  
+const util = require('../helper/util')
 
-module.exports.handler = async event => {
-    log.info(`delete`)    
+module.exports.handler = async event => { 
+    try {
+        const idObject = util.matchPathElements(event.detail.path, '/delete/{id}')
+        const params = {
+            Key: {
+                PK: {
+                    S: `USER#${idObject.id}`
+                },
+                SK: {
+                    S: `TYPE#ITEM1`
+                }
+            }, 
+            TableName: process.env.tableName
+        }
+    
+        const command = new DeleteItemCommand(params)
+        const metadata = await ddbClient.send(command)
+        log.info(metadata)
+    } catch (err) {
+        log.error(`error ${err}`)
+    }
 };

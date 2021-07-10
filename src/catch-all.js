9e1e8b1d8705dba83a2c14aef77cb94ceb59048a
nadtakanjones <nadtakan.jones@gmail.com>
2021-07-10 16:43:19 -0700

    optimized code

--- src/catch-all.js
+++ src/catch-all.js
@@ -1,29 +1,15 @@
 'use strict';
 const eventbridge = require('../helper/eventbridge');
-const log = require('lambda-log');
+const response = require('../helper/response-lib');
 
 module.exports.handler = async event => {
-	let err = null;
+    let err = null;
 	try {
         await eventbridge.sendToEventBridge(process.env.EVENT_BRIDGE, event);
     } catch (err) {
-        log.error(`error ${err}`);
         await sns.notifyFailure(err.errorMessage);
 	}
 	
 	const statusCode = err ? 500 : 200
-	const response = {
-		statusCode: statusCode,
-		headers: {
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Credentials": true
-		},
-		body: JSON.stringify(
-		{
-			statusCode: statusCode,
-		}
-	),
-	};
-  
-  return response
+	return response.buildResponse(statusCode)
 };

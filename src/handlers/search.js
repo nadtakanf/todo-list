b0b8e9040e262ed3bcd3f43d50f569da43c72234
nadtakanjones <nadtakan.jones@gmail.com>
2021-07-12 00:55:49 -0700

    added search by itemStatus

--- src/handlers/search.js
+++ src/handlers/search.js
@@ -1,6 +1,29 @@
 'use strict';
 const log = require('lambda-log');
+const { DynamoDBClient, QueryCommand } = require("@aws-sdk/client-dynamodb")
+const client = new DynamoDBClient({ region: process.env.REGION })  
+const sns = require('../helper/sns')
+const util = require('../helper/util')
 
 module.exports.handler = async event => {
-    log.info(`search`)
+    try {
+        const { claims } = event.detail.requestContext.authorizer;
+        const { itemStatus } = event.detail.multiValueQueryStringParameters;
+        
+        const params = {
+            TableName: process.env.TABLE_NAME,
+            IndexName: 'itemStatusGSI',
+            KeyConditionExpression: "itemStatus = :itemStatus and PK = :pk", 
+            ExpressionAttributeValues: {
+                ":itemStatus": { S: itemStatus[0] },
+                ":pk": { S: `USER#${claims['cognito:username']}` }
+            }
+        } 
+    
+        const command = new QueryCommand(params)
+        const response = await client.send(command);
+        return response.$metadata.Items
+    } catch (err) {
+        await sns.notifyFailure(err);
+    }
 };

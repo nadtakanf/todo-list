7eaf361a22e907ac7f4ba9c2bf887a2c61c447ac
nadtakanjones <nadtakan.jones@gmail.com>
2021-07-12 19:13:48 -0700

    clean up unuse code

--- src/handlers/create.js
+++ src/handlers/create.js
@@ -4,13 +4,10 @@ const client = new DynamoDBClient({ region: process.env.REGION })
 const util = require('../helper/util') 
 const response = require('../helper/response-lib')
 const sns = require('../helper/sns')
-const log = require('lambda-log')
 
 module.exports.handler = async event => {
 	try {
         const { body } = event.detail;
-        const { headers } = event.detail;
-
         const { claims } = event.detail.requestContext.authorizer;
 
         const params = util.formatItem(JSON.parse(body), claims)
@@ -18,7 +15,6 @@ module.exports.handler = async event => {
         const metadata = await client.send(command); 
         return response.success(metadata.$metadata.httpStatusCode)
     } catch (err) {
-        console.log(err)
         await sns.notifyFailure(err);
 	}
 };
